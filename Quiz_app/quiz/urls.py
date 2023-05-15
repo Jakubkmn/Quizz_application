4c26e8d4f1124afb535071b0657557217d5d777a
Jakub <jakubkmn12@gmail.com>
2023-05-15 21:17:01 +0200

    Adding loging to the page

--- Quiz_app/quiz/urls.py
+++ Quiz_app/quiz/urls.py
@@ -5,5 +5,7 @@ app_name = 'quiz'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('login/', views.login_user, name='login'),
+    #path('logout/', views.logout_user, name='logout'),
 
 ]
--- Quiz_app/quiz/views.py
+++ Quiz_app/quiz/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'quiz/home.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('quiz:home')
+        else:
+            messages.success(request, "There was an error logging in. Please try again")
+            return redirect('quiz:home')
+    else:
+        return render(request, 'quiz/home.html', {})
+
+def logout_user(request):
+    pass
